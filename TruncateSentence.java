68f9af7877511ac9de8bf7717d192c4b9be0a326
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-10-20 22:10:45 -0400

    Update TruncateSentence.java

--- TruncateSentence.java
+++ TruncateSentence.java
@@ -3,16 +3,20 @@
 class Solution {
     public String truncateSentence(String s, int k) {
         
+//         spilt entire sentence into an array
         String [] words = s.split(" ");
-        
+//         String builder , self explanatory
         StringBuilder sb = new StringBuilder();
+//         to hold new sentence
         String nSen ="";
-        
-        for(int i =0; i<k; i++){            
+//         loop k times 
+        for(int i =0; i<k; i++){   
+//             append each element + an empty space after 
             sb.append(words[i]+ " ");
+//             assign sb to nSen
              nSen = sb.toString();
 }
-        
+//         remove trailing spaces.
         return nSen.trim();
         
         
