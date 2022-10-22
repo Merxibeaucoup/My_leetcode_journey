f469fff6a7b29e75a5f926b48617949112a40bb0
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-10-21 23:04:02 -0400

    Create 1bit2bit.java

---
+++ 1bit2bit.java
@@ -0,0 +1,20 @@
+// 717. 1-bit and 2-bit Characters
+
+// L question , no bueno
+
+
+class Solution {
+  public boolean isOneBitCharacter(int[] bits) {
+    final int n = bits.length;
+
+    int i = 0;
+    while (i < n - 1)
+      if (bits[i] == 0)
+        i += 1;
+      else
+        i += 2;
+
+    return i == n - 1;
+  }
+}
+
