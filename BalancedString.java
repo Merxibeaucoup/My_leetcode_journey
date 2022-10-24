ffebfefe861051fedbecd213dfa11f39707a2514
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-10-23 22:48:22 -0400

    Create BalancedString.java

---
+++ BalancedString.java
@@ -0,0 +1,34 @@
+// 1221. Split a String in Balanced Strings
+
+// Brute force solution
+
+class Solution {
+    public int balancedStringSplit(String s) {
+        
+        int count =0;
+        int r =0;
+        int l = 0;
+        
+        for(int i =0; i<s.length(); i++){
+            
+            if(s.charAt(i) == 'R'){
+
+                r++;
+                
+            }
+            else {
+                
+                 l++;
+            }
+             if(r==l) {
+
+                 count++;
+                 r=0;
+                 l=0;
+             } 
+
+        }
+        return count;
+        
+    }
+}
