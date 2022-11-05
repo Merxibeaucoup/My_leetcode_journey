ebb233d8dfc758b6ed4c07ff8b727701c643de87
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-11-04 23:58:13 -0400

    Create NumGoodPairs.java

---
+++ NumGoodPairs.java
@@ -0,0 +1,55 @@
+// leetcode 1512. Number of Good Pairs
+
+/*
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+*/
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+
+        int res =0;
+        
+        /*
+            declare hashMap
+        */
+        HashMap<Integer, Integer> map = new HashMap();
+
+        /*
+           Loop through array
+        */
+        
+        for(int num : nums){
+            
+        /*
+            K,V
+            put pairs in map
+            num as key 
+            getOrDefault().... means get num and count it +1 , else num = 0
+
+        */
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+
+        for(int key: map.keySet()){
+            int val =map.get(key);
+
+            if(val>1){
+                    res+= (val *(val-1))/2;
+            }
+        }
+    
+
+return res;
+        
+    }
+}
