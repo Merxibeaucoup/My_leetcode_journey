cc1ef8d72023f6b1e3609cef72b85a25fece43f1
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-11-06 19:45:24 -0500

    Create MaxNumWords.java

---
+++ MaxNumWords.java
@@ -0,0 +1,28 @@
+// leetcode 2114. Maximum Number of Words Found in Sentences
+
+/*
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+Return the maximum number of words that appear in a single sentence.
+*/
+
+
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+
+        
+        int max_l =0;
+
+        for (int i =0; i < sentences.length; i++){
+                String [] sp = sentences[i].split(" ");
+                if(sp.length > max_l){
+                    max_l = sp.length;
+                }
+        }
+
+        return max_l;
+        
+    }
+}
