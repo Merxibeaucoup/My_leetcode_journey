6c20b8f6977acefb0a4bf936c59c87a08c43045d
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2022-11-07 20:57:22 -0500

    Create restoreString.java

---
+++ restoreString.java
@@ -0,0 +1,20 @@
+// leetcode 1528. Shuffle String
+
+/*
+You are given a string s and an integer array indices of the same length. 
+The string s will be shuffled such that the character at the ith 
+position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+*/
+
+class Solution {
+    public String restoreString(String s, int[] indices) {
+       
+        char[] res = new char[s.length()];
+        for(int i = 0;i<indices.length;i++)
+            res[indices[i]] = s.charAt(i);
+        return new String(res);
+    
+    }
+}
